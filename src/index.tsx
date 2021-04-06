16f6d63a83046a9c0b91d0b2df921fcf97bb97ca
flaaaps <julianwening4@gmail.com>
2021-04-06 20:29:20 +0200

    Move router

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom"
 import App from "@components/App"
 import "@styles/tailwind.css"
 import "@styles/index.css"
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById("root")
 )
