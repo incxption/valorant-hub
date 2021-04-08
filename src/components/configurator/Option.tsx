96d4084db0691a2e529aa838917a3050c5f95c10
flaaaps <julianwening4@gmail.com>
2021-04-08 20:23:19 +0200

    Simplify/add animations with motion

--- src/components/configurator/Option.tsx
+++ src/components/configurator/Option.tsx
@@ -1,24 +1,52 @@
 import React, { useState } from "react"
 import classNames from "classnames"
+import { motion } from "framer-motion"
 
 type Props = {
     label: JSX.Element | string
     selected: boolean
 
-    tooltip?: JSX.Element | string
+    tooltipText?: JSX.Element | string
     enabled?: boolean
     onClick?: () => void
 }
 
-const Option: React.FC<Props> = ({ label, tooltip, selected, enabled = true, onClick }) => {
+const Option: React.FC<Props> = ({ label, tooltipText, selected, enabled = true, onClick }) => {
     const [hovered, setHovered] = useState(false)
 
+    const button = {
+        selected: {
+            backgroundColor: "rgb(59, 130, 246)",
+            scale: 1,
+            color: "white"
+        },
+        initial: {
+            backgroundColor: "rgb(228, 228, 231)",
+            color: "rgb(39, 39, 42)",
+            scale: 0.95
+        }
+    }
+
+    const tooltip = {
+        hovered: {
+            scale: 1,
+            y: 0,
+            opacity: 1,
+            x: "-50%"
+        },
+        initial: { scale: 0, y: "100%", opacity: 1, x: "-50%" }
+    }
+
     return (
-        <div
+        <motion.div
+            variants={button}
+            animate={selected ? "selected" : "initial"}
+            initial="initial"
+            transition={{ duration: 0.15 }}
             className={classNames(
                 "inline-block bg-white mr-3 mb-3 px-2.5 py-1 rounded-md shadow-sm font-medium text-center " +
                     "cursor-pointer flex w-max items-center relative",
-                selected && "shadow-md bg-blue-500 text-white",
+                selected && "shadow-md",
                 !enabled && "bg-gray-200 cursor-not-allowed"
             )}
             onClick={() => enabled && onClick && onClick()}
@@ -26,21 +54,19 @@ const Option: React.FC<Props> = ({ label, tooltip, selected, enabled = true, onC
             onMouseLeave={() => setHovered(false)}
         >
             {label}
-            {tooltip && (
-                <div
+            {tooltipText && (
+                <motion.div
                     className="absolute left-1/2 -top-full w-max text-sm font-normal bg-white rounded-md px-2 py-1
-                               border border-gray-200 shadow-sm z-20 transition-all text-gray-700 cursor-default"
-                    style={{
-                        transform:
-                            (hovered ? "scale(1) translateY(0)" : "scale(0) translateY(100%)") + " translateX(-50%)",
-                        opacity: hovered ? 1 : 0,
-                        transformOrigin: "left"
-                    }}
+                               border border-gray-200 shadow-sm z-20 text-gray-700 cursor-default"
+                    variants={tooltip}
+                    animate={hovered ? "hovered" : "initial"}
+                    initial="initial"
+                    transition={{ duration: 0.15 }}
                 >
-                    {tooltip}
-                </div>
+                    {tooltipText}
+                </motion.div>
             )}
-        </div>
+        </motion.div>
     )
 }
 
